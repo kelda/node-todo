774acc8130b93543266da466d3f9c17da97927bf
Kevin Lin <kevin@kelda.io>
2020-09-03 13:25:58 -0700

    Fix 404 for index.html
    Going to / has always worked, but users would see scary "no such file or
    directory" log messages from automated requests to other endpoints
    (which don't actually affect the application).

--- app/routes.js
+++ app/routes.js
@@ -55,6 +55,6 @@ module.exports = function (app) {
 
     // application -------------------------------------------------------------
     app.get('*', function (req, res) {
-        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendFile(__dirname + '../public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
 };
